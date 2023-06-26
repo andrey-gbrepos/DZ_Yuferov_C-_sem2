17e22ae399ba4939c4a71edce77f8826e96240e6
andrey-gbrepose <yandire@yandex.ru>
2023-06-26 17:58:15 +0300

    Add Zadacha13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 13: Напишите программу, которая выводит 
+//третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+Console.WriteLine($"Программа выдаст третью цифру из заданного целого числа введенного Вами ");
+Console.WriteLine($"");
+Console.Write($"Введите целое число: ");
+
+int num = int.Parse(Console.ReadLine());
+
+int power = 10; //для определения порядка числа
+int s = num; // Временная переменная для цикла
+if (num < 100)
+    {
+        Console.WriteLine($"Число должно быть не меньше 100 ");
+        Console.Write($"Запустите программу еще раз. ");
+        return;
+    }
+
+while ( s >=10)
+{
+    s = s/10;
+    power = power*10;
+}
+//Console.WriteLine($"{power}");
+Console.WriteLine (" ");
+Console.WriteLine ($"Третья цифра -> {num/(power/1000)-(num/(power/100))*10} ");
+Console.WriteLine (" ");
